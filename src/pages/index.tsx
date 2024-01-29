116a5f3ad914a959f246ec45e22a15c487720c41
Nima Nekouei <nekoueinima@gmail.com>
2024-01-29 17:23:14 +0330

    feat: add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import PrimaryLayout from "@src/components/layouts/primary/primaryLayout";
+import HeroSection from "@src/components/pages/landing/hero/heroSection";
 import Navbar from "@src/components/pages/landing/navbar/navbar";
 import { NextPageWithLayout } from "./page";
 
@@ -6,6 +7,8 @@ const HomePage: NextPageWithLayout = () => {
     return (
         <div className="py-4 relative">
             <Navbar />
+            <HeroSection />
+
             {/* <section>Hero Section</section>
             <section>Cta</section>
             <footer>Footer</footer> */}
