ee97c61ba1bfcd030ea34489f2a02d9950f331a2
Nima Nekouei <nekoueinima@gmail.com>
2024-01-30 21:53:51 +0330

    feat: install eslint dependencies and fix some eslint issues

--- src/hooks/useToggle/useToggle.ts
+++ src/hooks/useToggle/useToggle.ts
@@ -1,5 +1,6 @@
 import { useCallback, useState } from "react";
 
+// eslint-disable-next-line no-unused-vars
 export type UseToggle = [boolean, (state?: boolean) => void];
 
 const useToggle = (initialState?: boolean): UseToggle => {
