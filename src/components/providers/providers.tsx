838fa6761ce1f8600e2d730bb95ed16fe6410bf1
Nima Nekouei <nekoueinima@gmail.com>
2024-01-30 18:19:42 +0330

    feat: install react query and its devtool and its provider component

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,13 +1,18 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import { FC, PropsWithChildren } from "react";
+import ReactQueryProvider from "./reactQueryProvider";
 
 interface IProvidersProps {}
 
 const Providers: FC<PropsWithChildren<IProvidersProps>> = ({ children }) => {
     const router = useRouter();
 
-    return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+    return (
+        <ReactQueryProvider>
+            <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+        </ReactQueryProvider>
+    );
 };
 
 export default Providers;
